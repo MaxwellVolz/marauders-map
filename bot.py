b02b402c975f650520269cc1ccfab05a814481f6
Maxwell Volz <maxwellvolz@gmail.com>
2024-08-10 19:38:48 -0700

    image map working with coordinate translations from json, including rotation and scale, really not performant

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import keyboard
 import cv2
 import numpy as np
 import os
+import json
 
 from PIL import ImageGrab
 
@@ -53,8 +54,8 @@ class MainController:
         minimap_np = np.array(minimap)
         minimap_cv = cv2.cvtColor(minimap_np, cv2.COLOR_BGR2RGB)
 
-        cv2.imshow("Matched Region", minimap)
-        cv2.waitKey(0)
+        # cv2.imshow("Matched Region", minimap)
+        # cv2.waitKey(0)
 
         return minimap_cv
 
@@ -69,16 +70,18 @@ class MainController:
         target_size = (550, 550)  # Example target size (width, height)
         minimap_resized = self.resize_image(minimap, target_size=target_size)
 
-        cv2.imshow("minimap_resized", minimap_resized)
-        cv2.waitKey(0)
+        # cv2.imshow("minimap_resized", minimap_resized)
+        # cv2.waitKey(0)
+
+        self.goblincave_scan(minimap_resized)
 
         # Step 2: Determine which map we are on
-        map_name = self.identify_map(minimap_resized)
-        print(f"Map identified: {map_name}")
+        # map_name = self.identify_map(minimap_resized)
+        # print(f"Map identified: {map_name}")
 
-        # Step 3: Determine location (placeholder for further implementation)
-        location = self.determine_location(minimap)
-        print(f"Location determined: {location}")
+        # # Step 3: Determine location (placeholder for further implementation)
+        # location = self.determine_location(minimap)
+        # print(f"Location determined: {location}")
 
     def identify_map(self, minimap):
         # Compare captured minimap with reference images
@@ -110,6 +113,102 @@ class MainController:
         cv2.waitKey(0)
 
         return max_val
+
+    def goblincave_scan(self, minimap):
+        img_path = "./images/all_levels"
+        goblin_img = "GoblinCave-5x5-01.png"
+        goblin_ref = cv2.imread(os.path.join(img_path, goblin_img))
+
+        # get location
+        result = cv2.matchTemplate(goblin_ref, minimap, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, max_loc = cv2.minMaxLoc(result)
+
+        # Optionally, draw a rectangle around the matched region for debugging
+        h, w = minimap.shape[:2]
+        cv2.rectangle(
+            goblin_ref, max_loc, (max_loc[0] + w, max_loc[1] + h), (0, 255, 0), 2
+        )
+
+        exact_max_loc = [max_loc[0] + 270, max_loc[1] + 270]
+        exact_h, exact_w = [10, 10]
+        cv2.rectangle(
+            goblin_ref,
+            exact_max_loc,
+            (exact_max_loc[0] + exact_w, exact_max_loc[1] + exact_h),
+            (0, 255, 0),
+            2,
+        )
+
+        # cv2.imshow("Matched Region", goblin_ref)
+        # cv2.waitKey(0)
+
+        print(f"h: {h} | w: {w}")
+        print(f"max_loc[0] + w: {max_loc[0] + w}")
+        print(f"max_loc[1] + h: {max_loc[1] + h}")
+
+        # Load the JSON data with points of interest
+
+        os.path.join(img_path, goblin_img)
+
+        with open("./data/GoblinCave-5x5-01-N-ez.json", "r") as f:
+            data = json.load(f)
+
+        # Map and plot the coordinates
+        for marker in data["markers"]:
+            # Assuming lat and lng correspond to the x, y on the image
+            # You might need to scale these according to your map's dimensions
+            lat = marker["coordinates"]["lat"]
+            lng = marker["coordinates"]["lng"]
+            marker_id = marker["id"]
+
+            img_h, img_w = goblin_ref.shape[:2]
+
+            scaled_lat, scaled_lng = scale_coordinates(
+                lat, lng, 0, 400, 0, 400, 2048, 2048
+            )
+
+            # Transform the coordinates based on the origin
+            transformed_lat, transformed_lng = transform_coordinates(
+                scaled_lat, scaled_lng, img_h, img_w, origin="clockwise"
+            )
+
+            print(f"transformed_lat: {transformed_lat}")
+            print(f"transformed_lng: {transformed_lng}")
+
+            # Plot the transformed marker on the minimap
+            cv2.circle(
+                goblin_ref,
+                (int(transformed_lat), int(transformed_lng)),
+                5,
+                (0, 0, 255),
+                -1,
+            )
+            cv2.putText(
+                goblin_ref,
+                marker_id,
+                (int(transformed_lat), int(transformed_lng) - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (255, 255, 255),
+                1,
+            )
+
+            # Plot the marker on the minimap
+            cv2.circle(goblin_ref, (int(lat), int(lng)), 5, (0, 0, 255), -1)
+            cv2.putText(
+                goblin_ref,
+                f"{marker_id}_og",
+                (int(lat), int(lng) - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (255, 255, 255),
+                1,
+            )
+
+        cv2.imshow("Matched Region", goblin_ref)
+        cv2.waitKey(0)
+
+        return
 
     def resize_image(self, image, scale=None, target_size=None):
         """Resize the image to a specific scale or target size."""
@@ -166,6 +265,52 @@ def start():
     """Start the bot."""
     controller = MainController()
     controller.run()
+
+
+def transform_coordinates(lat, lng, image_height, image_width, origin="top-left"):
+    if origin == "bottom-left":
+        # Flip vertically (y-axis)
+        transformed_lat = image_height - lat
+        transformed_lng = lng
+    elif origin == "top-right":
+        # Flip horizontally (x-axis)
+        transformed_lat = lat
+        transformed_lng = image_width - lng
+    elif origin == "bottom-right":
+        # Flip both axes
+        transformed_lat = image_height - lat
+        transformed_lng = image_width - lng
+    elif origin == "clockwise":
+        # Swap lat and lng and invert the new latitude to achieve a 90-degree clockwise rotation
+        transformed_lat = lng
+        transformed_lng = image_height - lat
+    elif origin == "counterclockwise":
+        # Swap lat and lng and invert the new longitude to achieve a 90-degree counterclockwise rotation
+        transformed_lat = image_width - lng
+        transformed_lng = lat
+    elif origin == "flip":
+        # Flip both axes (180-degree rotation)
+        transformed_lat = image_height - lat
+        transformed_lng = image_width - lng
+    else:  # 'top-left' is the default
+        transformed_lat = lat
+        transformed_lng = lng
+
+    return transformed_lat, transformed_lng
+
+
+def scale_coordinates(
+    lat, lng, lat_min, lat_max, lng_min, lng_max, image_height, image_width
+):
+    # Normalize the lat and lng to a 0-1 range
+    norm_lat = (lat - lat_min) / (lat_max - lat_min)
+    norm_lng = (lng - lng_min) / (lng_max - lng_min)
+
+    # Scale to the image dimensions
+    scaled_lat = norm_lat * image_height
+    scaled_lng = norm_lng * image_width
+
+    return scaled_lat, scaled_lng
 
 
 if __name__ == "__main__":
